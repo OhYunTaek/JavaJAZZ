6a2031bf974d104d8f1d6091de46fd53f433da31
OhYunTaek <dhdnjfgid@naver.com>
2022-09-22 17:53:02 +0900

    챕터 7 연습예제 내용

---
+++ Chap07/p291Bird.java
@@ -0,0 +1,14 @@
+/*
+ * Code By OhYunTaek 2022.09.22(목)
+ */
+public class p291Bird {
+    public void move() {
+        System.out.println("새가 움직인다~~~");
+    }
+}
+
+/*
+ * public interface Bird {
+ * void move();
+ * }
+ */
---
+++ Chap07/p292Member.java
@@ -0,0 +1,22 @@
+/*
+ * Code By OhYunTaek 2022.09.22(목)
+ */
+public class p292Member {
+    class Eagle extends p291Bird {
+        public void move() {
+            System.out.println("독수리가 난다~~~");
+        }
+
+        public void sound() {
+            System.out.println("독수~독수~");
+        }
+    }
+
+    Eagle e = new Eagle();
+
+    public static void main(String[] args) {
+        p292Member m = new p292Member();
+        m.e.move();
+        m.e.sound();
+    }
+}
